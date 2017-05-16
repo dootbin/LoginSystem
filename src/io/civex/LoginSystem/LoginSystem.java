89b3a135b80bab5627619d4988ec37707c151cf5
Ryan Reuther <ryan.reuther@gmail.com>
2017-05-15 18:18:59 -0700

    Initial commit

---
+++ src/io/civex/LoginSystem/LoginSystem.java
@@ -0,0 +1,24 @@
+package io.civex.LoginSystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Ryan on 5/15/2017.
+ */
+public class LoginSystem extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+
+
+}
