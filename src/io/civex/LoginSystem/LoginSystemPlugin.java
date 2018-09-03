4efd676a9da413ae65c84639f2f1ed80ea001c1d
Ryan Reuther <ryan.reuther@gmail.com>
2018-09-03 16:08:22 -0700

    IDK what this is but Loke asked for me to push the latest of what I had soo if it's not broken open an issue or make a PR

--- src/io/civex/LoginSystem/LoginSystemPlugin.java
+++ src/io/civex/LoginSystem/LoginSystemPlugin.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 /**
  * Created by Ryan on 5/15/2017.
+ * Test line to try to fix a local github ssl error where my stuff is out of sync.
  */
 public class LoginSystemPlugin extends JavaPlugin
 {
